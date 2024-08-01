7f91a6626a94936b31eea17268601140e1f97e5f
CristianUsiña <158125455+CrisLearn@users.noreply.github.com>
2024-07-31 20:14:14 -0500

    Login
    Ingreso desde el login con datos quemados al modulo de habitaciones

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,20 +1,20 @@
 // Datos quemados (por ejemplo, email y contraseña)
 const datosQuemados = {
-    email: 'usuario@ejemplo.com',
+    username: 'cristian',
     password: '123456'
   };
   
   // Controlador para manejar el login
   export const login = (req, res) => {
-    const { email, password } = req.body;
+    const { username, password } = req.body;
   
     // Validar los datos
-    if (email === datosQuemados.email && password === datosQuemados.password) {
+    if (username === datosQuemados.username && password === datosQuemados.password) {
       // Si los datos coinciden
       res.status(200).json({ mensaje: 'Login exitoso', success: true });
     } else {
       // Si los datos no coinciden
-      res.status(401).json({ mensaje: 'Email o contraseña incorrectos', success: false });
+      res.status(401).json({ mensaje: 'username o contraseña incorrectos', success: false });
     }
   };
   
